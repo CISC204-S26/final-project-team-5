f566a56b4035fcdfd0b4889e1aefdc5b61ddc2dc
tvn-ops <tvn@udel.edu>
2026-05-08 14:46:32 -0400

    Added a projectile and started working on making the first type of enemy fire the projectiles.

--- final_project_all_together_now/Scripts/enemy_type_1.gd
+++ final_project_all_together_now/Scripts/enemy_type_1.gd
@@ -1,12 +1,22 @@
 extends Node2D
-
+var firing = true
+signal fire
 func _process(_delta: float) -> void:
-	pass
+	if firing:
+		_throw_projectile()
 
 
 func _on_detection_radius_area_entered(_area: Area2D) -> void:
-	pass # Replace with function body.
+	firing = true
 
 
 func _on_hurtbox_area_entered(_area: Area2D) -> void:
 	queue_free()
+
+func _throw_projectile() -> void:
+	await get_tree().create_timer(4).timeout
+	fire.emit()
+
+
+func _on_detection_radius_area_exited(_area: Area2D) -> void:
+	firing = false
