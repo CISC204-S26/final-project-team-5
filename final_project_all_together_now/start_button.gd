6ac31b2f9f30b717291a126167b86bf74330c45d
esaylor1275 <esaylor1275@gmail.com>
2026-05-17 00:33:56 -0400

    wizard and witch now playable

---
+++ final_project_all_together_now/start_button.gd
@@ -0,0 +1,18 @@
+extends Button
+
+func _ready():
+	mouse_entered.connect(_on_hover)
+	mouse_exited.connect(_on_exit)
+	pressed.connect(_on_pressed)
+
+
+func _on_hover():
+	modulate = Color.DARK_CYAN      
+
+
+func _on_exit():
+	modulate = Color.WHITE    
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Scenes/scene2.tscn")
--- final_project_all_together_now/witch.gd
+++ final_project_all_together_now/witch.gd
@@ -6,33 +6,33 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 
-	#gravity
+	#garvity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	#whatsallthatmovement 
+	#aarowkey
 	var direction := 0
 
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(KEY_LEFT):
 		direction -= 1
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(KEY_RIGHT):
 		direction += 1
 
 	velocity.x = direction * speed
 
-	#flipthathoe
+	#flippy
 	if direction < 0:
 		$AnimatedSprite2D.flip_h = true
 	elif direction > 0:
 		$AnimatedSprite2D.flip_h = false
 
-	#jump
-	if Input.is_key_pressed(KEY_W) and is_on_floor():
+	# Jump (Up Arrow)
+	if Input.is_key_pressed(KEY_UP) and is_on_floor():
 		velocity.y = jump_force
 
 	move_and_slide()
 
-	#animation cycles
+	#animation
 	if direction != 0:
 		$AnimatedSprite2D.play("Walk")
 	else:
