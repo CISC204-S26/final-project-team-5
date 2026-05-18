744a7c160c90df5f6d4adbea85e8bb74f1f8ff00
YourAv3rageGamer <fatwhale497@outlook.com>
2026-05-17 23:01:49 -0400

    trying to get the portal to work

--- final_project_all_together_now/portal.gd
+++ final_project_all_together_now/portal.gd
@@ -1,9 +1,21 @@
 extends Area2D
 
+
 @export_file var NEXT_LEVEL: String = ""
 
-
+var players_in_portal := 0
 
 func _on_body_entered(body):
-	if body.is_in_group("Player") and NEXT_LEVEL !="":
-		get_tree().change_scene_to_file(NEXT_LEVEL)
+	if body.is_in_group("Player"):
+		players_in_portal += 1
+		print(players_in_portal)
+
+		if players_in_portal >= 2 and NEXT_LEVEL != "":
+			get_tree().change_scene_to_file(NEXT_LEVEL)
+
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		players_in_portal -= 1
